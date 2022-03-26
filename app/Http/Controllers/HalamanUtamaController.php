397d14e391ee15f8323b8c86201548fcf78bfd08
Nana Suryana <nana.suryana@wgs-id.com>
2022-03-27 01:58:33 +0700

    Bugfixed Auth Controller

--- app/Http/Controllers/HalamanUtamaController.php
+++ app/Http/Controllers/HalamanUtamaController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Alternatif;
+use Illuminate\Support\Facades\Auth;
 
 class HalamanUtamaController extends Controller
 {
     public function index()
     {
-        $cars = Alternatif::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->limit(3)->get();
+        $cars = Alternatif::orderBy('created_at', 'desc')->limit(3)->get();
         return view('halaman_utama.index')->with('cars', $cars);
     }
 }
