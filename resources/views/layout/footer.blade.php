b330c76c116b3828f0a8f82358265e3524fab96c
nana.suryana <nanasuryana554@gmail.com>
2022-08-21 16:06:21 +0700

    menambahkan link bantuan pada footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,9 @@
 <footer>
     <div class="text-center">
-        Develop by <a href="https://www.instagram.com/rhfstory/" target="_blank">Arif Irawan</a>
+        Untuk Penggunaan dan Petunjuk aplikasi pergi ke menu bantuan, atau <a href="{{ route('help.index') }}"
+            class="btn-link">Klik
+            Disini</a>
+        <br /><br />Develop by <a href="https://www.instagram.com/rhfstory/" target="_blank">Arif Irawan</a>
     </div>
     <div class="clearfix"></div>
 </footer>
